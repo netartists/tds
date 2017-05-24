b7025f98ea82600c9f120ee5b42e2f104c2ffeed
Rene Sonntag <rene.sonntag@t-systems.com>
2017-05-24 22:40:29 +0200

    - initial commit

--- wp-content/themes/CherryFramework/single-team.php
+++ wp-content/themes/CherryFramework/single-team.php
@@ -8,9 +8,6 @@
 					<div class="<?php echo cherry_get_layout_class( 'content' ); ?> <?php echo of_get_option('blog_sidebar_pos'); ?>" id="content" data-motopress-type="loop" data-motopress-loop-file="loop/loop-single-team.php">
 						<?php get_template_part("loop/loop-single-team"); ?>
 					</div>
-					<div class="<?php echo cherry_get_layout_class( 'sidebar' ); ?> sidebar" id="sidebar" data-motopress-type="static-sidebar"  data-motopress-sidebar-file="sidebar.php">
-						<?php get_sidebar(); ?>
-					</div>
 				</div>
 			</div>
 		</div>
